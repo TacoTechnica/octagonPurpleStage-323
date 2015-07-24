db8912b6265fd16c41e8dd328b8886615ad25273
adrisj7 <adrisj7@gmail.com>
2015-07-24 10:54:43 -0400

    Renaming

--- website.py
+++ website.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from util import reader,checker
 
 website=Flask(__name__)
-
-main_user = ""
-
 
 
 
 @website.route("/")
 def homepage():
-    if main_user == "":
-        return redirect("/login")
+    if not 'username' in session:
+        return redirect("login/")
     else:
-        return render_template("homepage.html",height = 400, title = 'OPS - Home',user = ", " + main_user)
+        return render_template("homepage.html",height = 400, title = 'OPS - Home',user = ", " + session['username'])
 
 ##############LOGIN AND ACCOUNTS#################
 
 @website.route( '/login/')
 def login():
-    return render_template("login.html",error = "")
+    if not 'username' in session:
+        return render_template("login.html",error = "")
+    else:
+        return redirect("/")
+
+
+@website.route( '/logout/')
+def logout():
+    if 'username' in session:
+        session.pop('username', None)
+    return redirect("/")
 
 
 @website.route( '/login/result', methods = ["POST"])
@@ -32,8 +39,8 @@ def result():
     if user == "" or pw == "":
         return render_template("login.html",error = "Both elements must be filled!")
     elif user in user_list.keys() and user_list[user] == pw:
-        main_user = user
-        print main_user + " ONE"
+        session['username'] = user
+        print "POTATO: " + session['username']
         return redirect("/")
     else:
         return render_template("login.html",error = "Incorrect username and/or password")
@@ -67,5 +74,10 @@ def registered():
 
 
 if __name__=="__main__":
+    
+    website.secret_key ="""@t/"Iq^7y5cV>`\'<Rlv"""
+    website.config['SESSION_TYPE'] = 'filesystem'
+    #sess = Session()
+    #sess.init_app(website)
     website.debug=True
     website.run(host="0.0.0.0",port=5000)
